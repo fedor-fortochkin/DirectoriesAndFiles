ca7497eddf575ca3096ab125d9b2fcac17f77016
Fedor Fortochkin <f_fortochkin@inbox.ru>
2020-01-13 02:05:30 +0300

    Правка багов

--- src/main/java/ru/fortochkin/directoriesandfiles/DirectoriesAndFilesRunner.java
+++ src/main/java/ru/fortochkin/directoriesandfiles/DirectoriesAndFilesRunner.java
@@ -16,18 +16,35 @@
 
 package ru.fortochkin.directoriesandfiles;
 
+import java.util.TimeZone;
+import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.MimeMappings;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
-public class DirectoriesAndFilesRunner {
+public class DirectoriesAndFilesRunner implements EmbeddedServletContainerCustomizer{
 
-	public static void main(String[] args) throws Exception {
-		SpringApplication.run(DirectoriesAndFilesRunner.class, args);
-	}
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(DirectoriesAndFilesRunner.class, args);
+    }
+
+    @Override
+    public void customize(ConfigurableEmbeddedServletContainer cesc) {
+        MimeMappings mappings = new MimeMappings(MimeMappings.DEFAULT);
+        mappings.add("html", "text/html;charset=utf-8");
+        cesc.setMimeMappings(mappings );
+    }
+    
+    @PostConstruct
+    public void init(){
+      TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
 
 }
--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -2,11 +2,14 @@ endpoint="/content/";
 rootContentTable="#rootContentList"
 dirContentTable='#dirContentList'
 
+function fileSizeSI(a,b,c,d,e){
+ return (b=Math,c=b.log,d=1e3,e=c(a)/c(d)|0,a/b.pow(d,e)).toFixed(2)
+ +' '+(e?'KMGTPEZY'[--e]+'b':'Bytes')
+}
 
 $(getRootContent())
 
 $('#addDirBtn').click(function(){
-  //jQuery.post( endpoint, $("#newDirectoryInput").val(),null,'text/plain',);
   $.ajax({
     url:endpoint,
     type:"POST",
@@ -24,7 +27,7 @@ function getRootContent(){
         console.log(data);
         $(rootContentTable+" tbody").empty();
         data.forEach(function(elem){
-            $(rootContentTable).append('<tr><td>'+elem.date+'</td><td>'+elem.baseDir+'</td><td>'+elem.dirsCount+'</td><td>'+elem.filesCount+'</td><td>'+elem.contentSize+'</td><td><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#dirContent" data-id="'+elem.id+'">Файлы</button></td></tr>');
+            $(rootContentTable).append('<tr><td>'+elem.date+'</td><td>'+elem.baseDir+'</td><td>'+elem.dirsCount+'</td><td>'+elem.filesCount+'</td><td>'+fileSizeSI(elem.contentSize)+'</td><td><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#dirContent" data-id="'+elem.id+'" data-title="'+elem.baseDir+' '+elem.date+'">Файлы</button></td></tr>');
         });
     })    
 }
@@ -32,16 +35,15 @@ function getRootContent(){
 $('#dirContent').on('show.bs.modal', function (event) {
     var button = $(event.relatedTarget);
     var id = button.data('id');
-    
+    var title = button.data('title');
     data = $.get(endpoint+id+"/",function(data){
-        console.log(data);
-        var modal = $(this);
-        modal.find('.modal-title').text('New message to ' + id);
-        console.log(modal.find('.modal-title'));
+
+        $('#modal-title').text(title);
+        
         $(dirContentTable+" tbody").empty();
 
         data.forEach(function(elem){
-            $(dirContentTable).append('<tr><td>'+elem.name+'</td><td>'+(elem.size === null ? '&lt;DIR&gt;' : elem.size)+'</td>');
+            $(dirContentTable).append('<tr><td>'+elem.name+'</td><td>'+(elem.size === null ? '&lt;DIR&gt;' : fileSizeSI(elem.size))+'</td>');
         });
     });    
 });
